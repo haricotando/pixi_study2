bb46f7fe287e86d9297d9642c4e7acc13d0fea33
Masaru ANDO <haricotando@gmail.com>
2024-01-16 14:48:20 +0900

    Easingを移植

--- helper/Easing.js
+++ helper/Easing.js
@@ -1,5 +1,5 @@
-
 class Easing {
+	
     static hello(){
         console.log('hello');
     }
@@ -9,34 +9,201 @@ class Easing {
         b: begInnIng value
         c: change In value
         d: duration
-    */
-
+    */	
+	
+	/*
+		Linear
+		---------------------------------------------------------------------------------
+	*/
 	static linear = this.easeLinear;
-	static easeLinear (t, b, c, d) {
+	static none = this.easeLinear;
+	static easeLinear (t, b, c, d){
 		return c*t/d + b;
 	}
 
-	static easeInOutQuad (t, b, c, d) {
+	/*
+	Sine
+	---------------------------------------------------------------------------------
+	*/
+	static easeInSine (t, b, c, d){
+		return -c * Math.cos(t/d * this.PI_D2) + c + b;
+	}
+	static easeOutSine (t, b, c, d){
+		return c * Math.sin(t/d * this.PI_D2) + b;
+	}
+	static easeInOutSine (t, b, c, d){
+		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+	}
+
+	/*
+	Quintic
+	---------------------------------------------------------------------------------
+	*/
+	static easeInQuint (t, b, c, d){
+		return c*(t/=d)*t*t*t*t + b;
+	}
+	static easeOutQuint (t, b, c, d){
+		return c*((t=t/d-1)*t*t*t*t + 1) + b;
+	}
+	static easeInOutQuint (t, b, c, d){
+		if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+		return c/2*((t-=2)*t*t*t*t + 2) + b;
+	}
+
+	/*
+	Quartic
+	---------------------------------------------------------------------------------
+	*/
+	static easeInQuart (t, b, c, d){
+		return c*(t/=d)*t*t*t + b;
+	}
+	static easeOutQuart (t, b, c, d){
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	}
+	static easeInOutQuart (t, b, c, d){
+		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	}
+
+	/*
+	Quadratic
+	---------------------------------------------------------------------------------
+	*/
+	static easeInQuad (t, b, c, d){
+		return c*(t/=d)*t + b;
+	}
+	static easeOutQuad (t, b, c, d){
+		return -c *(t/=d)*(t-2) + b;
+	}
+	static easeInOutQuad (t, b, c, d){
 		if ((t/=d/2) < 1) return c/2*t*t + b;
 		return -c/2 * ((--t)*(t-2) - 1) + b;
-	};
-	
-	static easeInOutExpo (t, b, c, d) {
+	}
+
+	/*
+	Exponential
+	---------------------------------------------------------------------------------
+	*/
+	static easeInExpo (t, b, c, d){
+		return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+	}
+	static easeOutExpo (t, b, c, d){
+		return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+	}
+	static easeInOutExpo (t, b, c, d){
 		if (t==0) return b;
 		if (t==d) return b+c;
 		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
 		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
 	}
+
+	/*
+	Elastic
+	---------------------------------------------------------------------------------
+	*/
+	static easeInElastic (t, b, c, d, a=undefined, p=undefined){
+		var s;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (!a || a < Math.abs(c)) { a=c; s=p/4; }
+		else s = p/this.PI_M2 * Math.asin (c/a);
+		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*this.PI_M2/p )) + b;
+	}
+	static easeOutElastic (t, b, c, d, a=undefined, p=undefined){
+		var s;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (!a || a < Math.abs(c)) { a=c; s=p/4; }
+		else s = p/this.PI_M2 * Math.asin (c/a);
+		return (a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*this.PI_M2/p ) + c + b);
+	}
+	static easeInOutElastic (t, b, c, d, a=undefined, p=undefined){
+		var s;
+		if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+		if (!a || a < Math.abs(c)) { a=c; s=p/4; }
+		else s = p/this.PI_M2 * Math.asin (c/a);
+		if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*this.PI_M2/p )) + b;
+		return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*this.PI_M2/p )*.5 + c + b;
+	}
+
+	/*
+	Circular
+	---------------------------------------------------------------------------------
+	*/
+	static easeInCircular (t, b, c, d){
+		return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+	}
+	static easeOutCircular (t, b, c, d){
+		return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+	}
+	static easeInOutCircular (t, b, c, d){
+		if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+		return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+	}
+
+	/*
+	Back
+	---------------------------------------------------------------------------------
+	*/
+	static easeInBack (t, b, c, d, s=1.70158){
+		return c*(t/=d)*t*((s+1)*t - s) + b;
+	}
+	static easeOutBack (t, b, c, d, s=1.70158){
+		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+	}
+	static easeInOutBack (t, b, c, d, s=1.70158){
+		if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+	}
+
+	/*
+	Bounce
+	---------------------------------------------------------------------------------
+	*/
+	static easeInBounce (t, b, c, d){
+		return c - easeOutBounce (d-t, 0, c, d) + b;
+	}
+	static easeOutBounce (t, b, c, d){
+		if ((t/=d) < (1/2.75)) {
+			return c*(7.5625*t*t) + b;
+		} else if (t < (2/2.75)) {
+			return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+		} else if (t < (2.5/2.75)) {
+			return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+		} else {
+			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+		}
+	}
+	static easeInOutBounce (t, b, c, d){
+		if (t < d/2) return easeInBounce (t*2, 0, c, d) * .5 + b;
+		else return easeOutBounce (t*2-d, 0, c, d) * .5 + c*.5 + b;
+	}
+
+	/*
+	Cubic
+	---------------------------------------------------------------------------------
+	*/
+	static easeInCubic (t, b, c, d){
+		return c*(t/=d)*t*t + b;
+	}
+	static easeOutCubic (t, b, c, d){
+		return c*((t=t/d-1)*t*t + 1) + b;
+	}
+	static easeInOutCubic (t, b, c, d){
+		if ((t/=d/2) < 1) return c/2*t*t*t + b;
+		return c/2*((t-=2)*t*t + 2) + b;
+	}
+
 }
+
+Easing.PI_M2 = Math.PI*2;
+Easing.PI_D2 = Math.PI/2;
 
 export default Easing;
 
 /*
-https://github.com/danro/tweenman-as3/blob/master/Easing.as
-https://github.com/danro/jquery-easing/blob/master/jquery.easing.js
+	何十年経ってもお世話になる Robert Penner イージング
+	JS移植の参考にしたAS3コード
+	https://github.com/danro/tweenman-as3/blob/master/Easing.as
 */
-
-// 何十年経ってもお世話になる Robert Penner イージング
 
 /*
 	TERMS OF USE - EASING EQUATIONS
